79e5a376164e50cc6015406da54c0ae169caf215
tursunalikhorunaliyev <khorunaliyev@gmail.com>
2023-02-19 14:38:05 +0500

    turssunali

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 void main(List<String> args) {
   runApp(const MaterialApp(
-    home: LoginScreen(),
+    home: WellcomeScreen(),
     debugShowCheckedModeBanner: false,
   ));
 }
