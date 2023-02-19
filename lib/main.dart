8264a65fc4c638b6cdab634fc2f26f87d1289080
Shahzodbek2005 <shahzodbekkomilov2005@gmail.com>
2023-02-19 15:16:22 +0500

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
-import 'package:flinger/screens/first_screen.dart';
 import 'package:flinger/screens/login_screen.dart';
-import 'package:flinger/screens/splash_screen.dart';
-import 'package:flinger/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
   runApp(const MaterialApp(
-    home: WellcomeScreen(),
+    home: LoginScreen(),
     debugShowCheckedModeBanner: false,
   ));
 }
